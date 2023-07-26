e244307371d41f0bfec0f2e01754c75efbf6952b
kel <sirqueirakelly@gmail.com>
2023-07-26 19:17:39 -0300

    CheckBoxAssertions

--- src/main/java/CheckBoxAssertions.java
+++ src/main/java/CheckBoxAssertions.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class CheckBoxAssertions {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class CheckBoxAssertions {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "./src/main/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get(" https://rahulshettyacademy.com/dropdownsPractise/");
+        Assert.assertFalse(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+        System.out.println(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+
+        driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).click();
+        Assert.assertTrue(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+        System.out.println(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+
+        System.out.println(driver.findElements(By.cssSelector("input[type='checkbox']")).size());
+        driver.quit();
+    }
 }
