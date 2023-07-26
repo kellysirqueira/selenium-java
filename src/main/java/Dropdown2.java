c450e570c1fdb3ecaf1b81bca3231748201bd168
kel <sirqueirakelly@gmail.com>
2023-07-26 19:16:31 -0300

    Dropdown2

--- src/main/java/Dropdown2.java
+++ src/main/java/Dropdown2.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Dropdown2 {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Dropdown2 {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "./src/main/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get(" https://rahulshettyacademy.com/dropdownsPractise/");
+        driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+        driver.findElement(By.xpath("//a[@value='BLR']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//div[@id='ctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='MAA']")).click();
+        driver.close();
+        driver.quit();
+    }
 }
